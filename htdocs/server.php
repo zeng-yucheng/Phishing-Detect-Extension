221f210e8f374ab068d7e6801a10634b18b7a670
Zeng Yucheng <yucheng.zeng.1@outlook.com>
2022-03-23 23:09:04 +0800

    return results

--- htdocs/server.php
+++ htdocs/server.php
@@ -4,5 +4,6 @@ error_reporting(0);
 $url = $_POST["url"];
 // $res = exec("cmd.exe /k cd ../..", $out, $return);
 $result = exec("cmd.exe /k python ../../../NUS/S2/CS5331/Proj/proj/Phishing-Detect-Extension/script/main.py $url 2>&1", $output, $return);
-echo $output[1];
+$idx = sizeof($output) - 3;
+echo $output[$idx];
 ?>
